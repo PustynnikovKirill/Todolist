9a5931ff7dc76c8bcd0bab54aee11aefaed28ad5
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-03-18 14:31:41 +0300

    'map'

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,9 +1,7 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
-import {FilterValuesType} from './App';
-import {FullInput} from "./components/FullInput";
+import React from 'react';
 
 type TaskType = {
-    id: string
+    id: number
     title: string
     isDone: boolean
 }
@@ -11,40 +9,26 @@ type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskId: string) => void
-    changeFilter: (value: FilterValuesType) => void
-    addTask: (title: string) => void
 }
 
 export function Todolist(props: PropsType) {
-
-
-
-    const onAllClickHandler = () => props.changeFilter("all");
-    const onActiveClickHandler = () => props.changeFilter("active");
-    const onCompletedClickHandler = () => props.changeFilter("completed");
-
     return <div>
         <h3>{props.title}</h3>
-        <FullInput callBack={props.addTask} />
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
         <ul>
-            {
-                props.tasks.map(t => {
-
-                    const onClickHandler = () => props.removeTask(t.id)
-
-                    return <li key={t.id}>
-                        <input type="checkbox" checked={t.isDone}/>
-                        <span>{t.title}</span>
-                        <button onClick={ onClickHandler }>x</button>
-                    </li>
-                })
-            }
+            {props.tasks.map((el, index) => {
+                return (
+                <li key={index}><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                )
+            })}
         </ul>
         <div>
-            <button onClick={ onAllClickHandler }>All</button>
-            <button onClick={ onActiveClickHandler }>Active</button>
-            <button onClick={ onCompletedClickHandler }>Completed</button>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
         </div>
     </div>
 }
