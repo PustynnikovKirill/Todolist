778fca023cb9c3093c528b71390520cc06d2cc10
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-03-24 10:00:06 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
 import './App.css'
 
+type moneyTape = {
+    banknote: string,
+    nominal: number,
+    number: string
+}
 export const App = () => {
     let [money, setMoney] = useState([
         {banknote: 'dollar', nominal: 100, number: 'a12342123'},
@@ -15,15 +20,17 @@ export const App = () => {
 
     return (
         <ul>
-            {money.map((objFromMoneyArr) => {
-                return (
-                    <li>
-                        <span>{objFromMoneyArr.banknote}</span>
-                        <span>{objFromMoneyArr.banknote}</span>
-                        <span>{objFromMoneyArr.banknote}</span>
-                    </li>
+            {money.map((el:moneyTape,index)=>{
+            return (
+                <li >
+                    <span>{el.banknote} </span>
+                    <span>{el.nominal} </span>
+                    <span>{el.number} </span>
+                </li>
                 )
             })}
+
+
         </ul>
     )
 }
