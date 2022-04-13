3d1bd152b04408bcc412ec58fabadf7f581930ba
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-04-13 14:16:12 +0300

    'h'

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
-import { v1 } from 'uuid';
+import {v1} from 'uuid';
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
     let [tasks, setTasks] = useState([
-        { id: v1(), title: "HTML&CSS", isDone: true },
-        { id: v1(), title: "JS", isDone: true },
-        { id: v1(), title: "ReactJS", isDone: false },
-        { id: v1(), title: "Rest API", isDone: false },
-        { id: v1(), title: "GraphQL", isDone: false },
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false},
+        {id: v1(), title: "GraphQL", isDone: false},
     ]);
+    let [filter, setFilter] = useState<FilterValuesType>("all");
+
 
     function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id != id);
@@ -21,12 +23,20 @@ function App() {
     }
 
     function addTask(title: string) {
-        let task = { id: v1(), title: title, isDone: false };
+        let task = {id: v1(), title: title, isDone: false};
         let newTasks = [task, ...tasks];
         setTasks(newTasks);
     }
 
-    let [filter, setFilter] = useState<FilterValuesType>("all");
+    function changeStatus(taskId: string, isDone: boolean) {
+        let task = tasks.find(t => t.id === taskId);
+        if (task) {
+            task.isDone = isDone;
+        }
+
+        setTasks([...tasks]);
+    }
+
 
     let tasksForTodolist = tasks;
 
@@ -40,10 +50,6 @@ function App() {
     function changeFilter(value: FilterValuesType) {
         setFilter(value);
     }
-    const checkboxTaskHandler =(checkId:string, event:boolean)=>{
-        setTasks(tasks.map(el=>el.id === checkId ? {...el,isDone:event} : el))
-
-    }
 
 
     return (
@@ -53,7 +59,8 @@ function App() {
                       removeTask={removeTask}
                       changeFilter={changeFilter}
                       addTask={addTask}
-                      checkboxTaskHandler={checkboxTaskHandler}
+                      changeTaskStatus={changeStatus}
+                      filter={filter}
             />
         </div>
     );
