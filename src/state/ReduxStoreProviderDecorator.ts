58c29a803b58413ed78db54f9ff79747cccad3b7
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-06-29 22:04:31 +0300

    'dg'

--- src/state/ReduxStoreProviderDecorator.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {Provider} from "react";
-import {store} from "./store";
-
-
-export const ReduxStoreProviderDecorator = (storyFn: any) => {
-   return <Provider store = {store}> {storyFn()} </Provider>
-}
---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+
+export default {
+    title: 'API'
+}
+const settings = {
+    withCredentials: true,
+    headers: {
+        "API-KEY": "21415fcd-4311-4d9a-96d8-30d122f29a48"
+    }
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        // здесь мы будем делать запрос и ответ закидывать в стейт.
+        // который в виде строки будем отображать в div-ке
+        axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
+            .then((res) => {
+                debugger;
+                setState(res.data)
+            })
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title: "Kirill todolist"}, settings)
+            .then((res) => {
+
+                setState(res.data)
+            })
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.delete("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
