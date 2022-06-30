e679d78862c8cd52d1856e46570fade61a156b7f
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-06-30 20:44:45 +0300

    'api'

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -4,51 +4,51 @@ import axios from "axios";
 export default {
     title: 'API'
 }
+
 const settings = {
-    withCredentials: true,
+    withCredentials:true,
     headers: {
-        "API-KEY": "21415fcd-4311-4d9a-96d8-30d122f29a48"
+        'API-KEY': '720c39fd-3ebb-4793-a804-6ab459e83733'
     }
 }
 
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
+
     useEffect(() => {
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
         axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
-            .then((res) => {
-                debugger;
+            .then((res)=>{
                 setState(res.data)
             })
+
     }, [])
     return <div>{JSON.stringify(state)}</div>
 }
-
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title: "Kirill todolist"}, settings)
-            .then((res) => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: "newTodolist"}, settings).then( (res) => {
+            setState(res.data);
+        } )
 
-                setState(res.data)
-            })
     }, [])
+
     return <div>{JSON.stringify(state)}</div>
 }
-
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        axios.delete("https://social-network.samuraijs.com/api/1.1/todo-lists", settings)
-            .then((res) => {
-                setState(res.data)
-            })
+        const todolistId = 'e30a0d1b-013d-4eb6-8c44-8652ff151cf9';
+        axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, settings).then( (res) => {
+            setState(res.data);
+        })
+
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
-
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
