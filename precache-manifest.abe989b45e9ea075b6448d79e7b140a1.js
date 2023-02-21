f48751fbe589ee01cc3e68d7e606a9c26bcfee6d
Pustynnikov Kirill <kiryuha911@yandex.by>
2023-02-21 13:24:06 +0300

    Updates

---
+++ precache-manifest.abe989b45e9ea075b6448d79e7b140a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e36f2d87d511b107bc249269221c79",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "d51e13b6e0661a83363a",
+    "url": "/Todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/Todolist/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51e13b6e0661a83363a",
+    "url": "/Todolist/static/js/main.e89d1772.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
