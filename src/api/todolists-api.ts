05e24ed23bd5f6b435f0cddbc57115236601692a
Pustynnikov Kirill <kiryuha911@yandex.by>
2023-02-26 15:12:31 +0300

    'end'

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ import {GetTasksResponse, LoginParamsType, ResponseType, TaskType, TodolistType,
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '854b8180-5cc0-4505-a2c6-2abb4e98dc20'
     }
 }
 const instance = axios.create({
